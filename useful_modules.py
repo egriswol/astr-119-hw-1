ae7a9c9f3e198fc4ccb612e1e8b00848b05ce0ca
egriswol <43654429+egriswol@users.noreply.github.com>
2018-10-11 00:17:41 -0700

    Create useful_modules.py

---
+++ useful_modules.py
@@ -0,0 +1,9 @@
+import numpy as np				
+import matplotlib.pyplot as plt		#matplotlib's pyplot
+
+import sys							#gives access to c-like system library
+import os							#gives access to operating system
+
+
+print(sys.argv)						#prints any command line arguments including program name
+print(os.getcwd())					#prints the current working directory
